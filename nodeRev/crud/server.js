9a2a06bd9f8ef9cc7792cff20202d2846a7cf8f0
Deh1301 <calixtoandre77@gmail.com>
2025-08-11 19:24:33 -0300

    atualizando crud

--- nodeRev/crud/server.js
+++ nodeRev/crud/server.js
@@ -5,8 +5,8 @@ const app = express()
 app.use(express.json())
 
 let produto = [
-    {id: 1, nome: "Ps4", valor:2340},
-    {id: 2 , nome: "Notebook", valor:3000}
+    {id: 1, nome: "Ps4", preco:2340},
+    {id: 2 , nome: "Notebook", preco:3000}
 ]
 
 app.get("/produtos",(req,res)=>{
@@ -15,11 +15,35 @@ app.get("/produtos",(req,res)=>{
 })
 
 app.get("/produtos/:id",(req,res)=>{
-    const id = parseInt(req.params.id)
-    const retorno = produto.find(p=> p.id === id)
+    const Ids = parseInt(req.params.id)
+    const retorno = produto.find(p=> p.id === Ids)
+
+    if(!retorno){
+        return res.status(404).json({error:"Erro id nao localizado"})
+    }
 
     res.json(retorno)
 })
+
+app.post("/produtos", (req,res)=>{
+    const {nome, preco} = req.body
+
+    if(!nome || !preco){
+        return res.status(400).json({error:"nome e preço obrigatorio"})
+    }
+
+    const novoProduto = {
+        id:produto.length +1,
+        nome,
+        preco
+    }
+    produto.push(novoProduto)
+    res.status(201).json(novoProduto)
+
+})
+
+
+
 
 app.listen(3005,()=>{
     console.log("Servidor Rodando em http://localhost:3005")
